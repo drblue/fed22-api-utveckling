b8768b98b791a36540093704c729b77e3037ff20
Johan Nordström <johan@digitalvillage.se>
2023-01-10 14:15:41 +0100

    Respond to requests 💪🏻

--- 02-express/server.js
+++ 02-express/server.js
@@ -9,6 +9,16 @@ const PORT = 3000
 // Create a new Express app
 const app = express()
 
+// GET /
+app.get('/', (req, res) => {
+	res.send("Oh, hi there ☺️")
+})
+
+// GET /coffee
+app.get('/coffee', (req, res) => {
+	res.send("Is good for you!")
+})
+
 // Start listening for incoming requests on port 3000
 app.listen(PORT, () => {
 	console.log(`🥳 Yay, server started on localhost:${PORT}`)
