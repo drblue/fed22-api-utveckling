890c21fae68c11a027c8b2fbc7383139fd43529e
Johan Nordström <johan@digitalvillage.se>
2023-01-10 11:12:09 +0100

    Global-object in node.

---
+++ 01-basics/01-globals.js
@@ -0,0 +1,21 @@
+/***
+ * Globals
+ */
+
+/*
+setTimeout(() => {
+	console.log("I'll be back!");
+}, 3000);
+*/
+
+// console.log("Hello, world 🌱🌍!");
+
+/*
+// Will continue running until cancelled (Ctrl+C on both macOS and Windows)
+setInterval(() => {
+	console.log("Hello");
+}, 2000);
+*/
+
+console.log("Absolute path to directory for this file:", __dirname);
+console.log("Absolute path to this file:", __filename);
