4617bf52125d83f87bb78090ccc0f0618465e245
Johan Nordström <johan@digitalvillage.se>
2023-01-20 14:21:22 +0100

    Different Prisma-queries for retrieving/reading data.

--- 04-prisma/script.ts
+++ 04-prisma/script.ts
@@ -5,9 +5,44 @@ const main = async () => {
 	// Write Prisma Client queries here
 	console.log("It works?")
 
+	/*
+	// Get all phones and console.log them
+	const phones = await prisma.phones.findMany({    // SELECT manufacturer, model FROM phones
+		// select: {
+		// 	manufacturer: true,
+		// 	model: true,
+		// },
+		where: {
+			manufacturer: "Apple",
+		},
+	})
+	console.log("Phones:", phones)
+	*/
+
+	/*
 	// Get all users and console.log them
 	const users = await prisma.users.findMany()
-	console.log(users)
+	console.log("Users:", users)
+	*/
+
+	/*
+	// Get the _first_ user that matches our query
+	const user = await prisma.users.findFirst({
+		where: {
+			// id: 2,
+			name: "Korben Dallas",
+		}
+	})
+	console.log("User:", user)
+	*/
+
+	// Get a specific user
+	const user = await prisma.users.findUnique({
+		where: {
+			id: 4,
+		}
+	})
+	console.log("User:", user)
 }
 
 main()
