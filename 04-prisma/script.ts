d29d5415ddf6f55b6e767e130992efcc6d1cc568
Johan Nordström <johan@digitalvillage.se>
2023-01-20 15:18:20 +0100

    Query relations.

--- 04-prisma/script.ts
+++ 04-prisma/script.ts
@@ -26,6 +26,30 @@ const main = async () => {
 	*/
 
 	/*
+	// Get all users who's name starts with "Th" and console.log them
+	const users = await prisma.users.findMany({
+		where: {
+			name: {
+				// startsWith: "Th",
+				// endsWith: "an",
+				// contains: "an",
+			},
+		},
+		orderBy: [
+			{
+				name: 'asc',
+			},
+			{
+				id: 'desc',
+			},
+		],
+		take: 2,
+		skip: 1,
+	})
+	console.log("Users:", users)
+	*/
+
+	/*
 	// Get the _first_ user that matches our query
 	const user = await prisma.users.findFirst({
 		where: {
@@ -36,6 +60,7 @@ const main = async () => {
 	console.log("User:", user)
 	*/
 
+	/*
 	// Get a specific user
 	const user = await prisma.users.findUnique({
 		where: {
@@ -43,6 +68,38 @@ const main = async () => {
 		}
 	})
 	console.log("User:", user)
+	*/
+
+	/*
+	// Get a specific user and their phone
+	const user = await prisma.users.findUnique({
+		where: {
+			id: 2,
+		},
+		include: {
+			phones: true,
+		}
+	})
+	console.log("User:", user)
+	*/
+
+	/*
+	// Get all users and their phone(s)
+	const users = await prisma.users.findMany({
+		include: {
+			phones: true,
+		}
+	})
+	console.dir(users, { depth: null })
+	*/
+
+	// Get all phones and their user (if they have one)
+	const phones = await prisma.phones.findMany({
+		include: {
+			users: true,
+		}
+	})
+	console.log(phones)
 }
 
 main()
