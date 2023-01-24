8ec7aaea67a3485d96ab4a243a73ed8d1f695300
Johan Nordström <johan@digitalvillage.se>
2023-01-24 15:15:17 +0100

    Updated Prisma-schema with Book <n:n> Author relation. Also added routes for getting all authors and all books.

--- 05-prisma-books/src/app.ts
+++ 05-prisma-books/src/app.ts
@@ -15,4 +15,28 @@ app.get('/', (req, res) => {
 	})
 })
 
+/**
+ * GET /authors
+ */
+app.get('/authors', async (req, res) => {
+	try {
+		const authors = await prisma.author.findMany()
+		res.send(authors)
+	} catch (err) {
+		res.status(500).send({ message: "Something went wrong" })
+	}
+})
+
+/**
+ * GET /books
+ */
+app.get('/books', async (req, res) => {
+	try {
+		const books = await prisma.book.findMany()
+		res.send(books)
+	} catch (err) {
+		res.status(500).send({ message: "Something went wrong" })
+	}
+})
+
 export default app
