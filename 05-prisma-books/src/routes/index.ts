d5f467d7cb92237bd8f7ad3cbf3db5c7a9c8a503
Johan Nordström <johan@digitalvillage.se>
2023-01-31 12:07:33 +0100

    Added register route and register controller + mini-workshop.

--- 05-prisma-books/src/routes/index.ts
+++ 05-prisma-books/src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import { body } from 'express-validator'
 import authors from './authors'
 import books from './books'
 import publishers from './publishers'
+import { register } from '../controllers/register_controller'
 
 // instantiate a new router
 const router = express.Router()
@@ -29,5 +31,18 @@ router.use('/books', books)
  * /publishers
  */
 router.use('/publishers', publishers)
+
+/**
+ * /register
+ *
+ * @todo add validation rules for name, email and password
+ */
+router.post('/register', [
+	// place validation rules here
+
+	// name required + at least 3 chars
+	// email required + valid email
+	// password required + at least 6 chars
+], register)
 
 export default router
