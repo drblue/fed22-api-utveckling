89b2e2adb68098d8d571ee0078f74e479b071935
Johan Nordström <johan@digitalvillage.se>
2023-01-31 15:40:28 +0100

    Hash plaintext-password using bcrypt and create user in database.

--- 05-prisma-books/src/controllers/register_controller.ts
+++ 05-prisma-books/src/controllers/register_controller.ts
@@ -1,6 +1,7 @@
 /**
  * Register Controller
  */
+import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
 import { matchedData, validationResult } from 'express-validator'
 import prisma from '../prisma'
@@ -20,13 +21,29 @@ export const register = async (req: Request, res: Response) => {
 
 	// Get only the validated data from the request
 	const validatedData = matchedData(req)
-	console.log("req.body:", req.body)
 	console.log("validatedData:", validatedData)
 
 	// Calculate a hash + salt for the password
+	const hashedPassword = await bcrypt.hash(validatedData.password, Number(process.env.SALT_ROUNDS) || 10)
+	console.log("Hashed password:", hashedPassword)
+
+	// Replace password with hashed password
+	validatedData.password = hashedPassword
 
 	// Store the user in the database
+	try {
+		const user = await prisma.user.create({
+			data: {
+				name: validatedData.name,
+				email: validatedData.email,
+				password: validatedData.password,
+			},
+		})
 
-	// Respond with 201 Created + status success
-	res.status(201).send({ "status": "success", "data": req.body })
+		// Respond with 201 Created + status success
+		res.status(201).send({ status: "success", data: user })
+
+	} catch (err) {
+		return res.status(500).send({ status: "error", message: "Could not create user in database" })
+	}
 }
