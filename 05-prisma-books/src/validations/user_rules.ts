c94d297443cb67604a5f1efa6fa7b5602c043571
Johan Nordström <johan@digitalvillage.se>
2023-02-02 10:19:40 +0100

    Custom Validator for checking that an email does not already exist in the User-model.

--- 05-prisma-books/src/validations/user_rules.ts
+++ 05-prisma-books/src/validations/user_rules.ts
@@ -2,10 +2,23 @@
  * Validation Rules for User resource
  */
 import { body } from 'express-validator'
+import prisma from '../prisma'
 
 export const createUserRules = [
 	body('name').isString().bail().isLength({ min: 3 }),
-	body('email').isEmail(),
+	body('email').isEmail().custom(async value => {
+		// check if a User with that email already exists
+		const user = await prisma.user.findUnique({
+			where: {
+				email: value,
+			}
+		})
+
+		if (user) {
+			// user already exists, throw a hissy-fit
+			return Promise.reject("Email already exists")
+		}
+	}),
 	body('password').isString().bail().isLength({ min: 6 }),
 ]
 
