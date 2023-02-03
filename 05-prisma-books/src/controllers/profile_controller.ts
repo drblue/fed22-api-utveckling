19433ee6343a1e845485523a9058e11bf1ea1c72
Johan Nordström <johan@digitalvillage.se>
2023-02-03 12:17:08 +0100

    Send the user's details back to them on `GET /profile`

--- 05-prisma-books/src/controllers/profile_controller.ts
+++ 05-prisma-books/src/controllers/profile_controller.ts
@@ -1,16 +1,24 @@
 /**
  * Profile Controller
  */
+import Debug from 'debug'
 import { Request, Response } from 'express'
 import prisma from '../prisma'
+const debug = Debug("prisma-books:profile_controller")
 
 /**
  * Get the authenticated user's profile
  */
 export const getProfile = async (req: Request, res: Response) => {
+	// User has authenticated successfully
+
 	res.send({
 		status: "success",
-		data: null,
+		data: {
+			id: req.user.id,
+			name: req.user.name,
+			email: req.user.email,
+		},
 	})
 }
 
