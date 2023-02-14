489310ae436b516f1b32e3e4c71fd2afbbe89d0d
Johan Nordström <johan@digitalvillage.se>
2023-02-14 09:24:00 +0100

    Lousy Movie DB boilerplate.

---
+++ 06-lmdb/src/routes.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+// instantiate a new router
+const router = express.Router()
+
+/**
+ * GET /
+ */
+router.get('/', (req, res) => {
+	res.send({
+		message: "I AM MOVIE-DB-API, GIVES POPCORN",
+	})
+})
