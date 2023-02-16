b6b1b8dc8a40ab60cd74c8c0b433eb3aed086e5b
Johan Nordström <johan@digitalvillage.se>
2023-02-16 10:40:23 +0100

    Mongoose schema types, sanitization, validation, custom validation, default values.

--- 06-lmdb/src/resources/movie/movie.model.ts
+++ 06-lmdb/src/resources/movie/movie.model.ts
@@ -2,14 +2,47 @@ import { model, Schema, Document } from 'mongoose'
 
 export interface IMovie extends Document {
 	title: string,
-	runtime?: number,
+	runtime: number | null,
 	releaseYear?: number,
+	genres: string[],
+	watched?: Date,
 }
 
 const MovieSchema: Schema = new Schema<IMovie>({
-	title: { type: String, required: true },
-	runtime: { type: Number, min: 1 },
-	releaseYear: { type: Number, min: 1888 },
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 3,
+		unique: true,
+	},
+	runtime: {
+		type: Number,
+		default: null,
+		// min: 1,
+		validate(value: number) {
+			if (value < 1 && value !== null) {
+				throw new Error("Just because you thought the movie was bad, it shouldn't have a zero or negative runtime.")
+			}
+		},
+	},
+	releaseYear: {
+		type: Number,
+		min: 1888,
+		max: new Date().getFullYear(),
+	},
+	genres: {
+		type: [String],
+		lowercase: true,
+		default: [],
+		// enum: ["action", "sci-fi", "bromance", "realism"],
+	},
+	watched: {
+		type: Date,
+		default() {
+			return Date.now()
+		},
+	}
 })
 
 export const Movie = model<IMovie>('Movie', MovieSchema)
