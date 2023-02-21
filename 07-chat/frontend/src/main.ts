e22bf5ad84e701157cf2003f7fe965fc3c710b35
Johan Nordström <johan@digitalvillage.se>
2023-02-21 11:18:40 +0100

    Connect to our Socket.IO server. Also listen for connects and disconnects.

--- 07-chat/frontend/src/main.ts
+++ 07-chat/frontend/src/main.ts
@@ -1,5 +1,21 @@
 import './assets/scss/style.scss'
+import { io, Socket } from 'socket.io-client'
+
+const SOCKET_HOST = import.meta.env.VITE_APP_SOCKET_HOST
 
 const messageEl = document.querySelector('#message') as HTMLInputElement
 const messageFormEl = document.querySelector('#message-form') as HTMLFormElement
 const messagesEl = document.querySelector('#messages') as HTMLDivElement
+
+// Connect to Socket.IO server
+const socket = io(SOCKET_HOST)
+
+// Listen for when connection is established
+socket.on('connect', () => {
+	console.log('💥 Connected to the server', socket.id)
+})
+
+// Listen for when the server got tired of us
+socket.on('disconnect', () => {
+	console.log('💀 Disconnected from the server')
+})
