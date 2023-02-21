b4b182cb4822b0fdd88b3f346b9f6657cd76acc6
Johan Nordström <johan@digitalvillage.se>
2023-02-21 10:49:47 +0100

    Removed types I forgot to remove 😅.

--- 07-chat/backend/src/types/shared/SocketTypes.ts
+++ 07-chat/backend/src/types/shared/SocketTypes.ts
@@ -2,13 +2,10 @@ export {}
 
 // Events emitted by the server to the client
 export interface ServerToClientEvents {
-	hello: () => void
-	newChatMessage: (data: ChatMessageData) => void
 }
 
 // Events emitted by the client to the server
 export interface ClientToServerEvents {
-	sendChatMessage: (data: ChatMessageData) => void
 }
 
 // Events between servers
@@ -17,7 +14,4 @@ export interface InterServerEvents {
 
 // Message payload
 export interface ChatMessageData {
-	content: string
-	// timestamp: number
-	// username: string
 }
