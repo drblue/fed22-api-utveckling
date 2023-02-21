7408f6200085a67ee7ecfc1490df7deb6bea1649
Johan Nordström <johan@digitalvillage.se>
2023-02-21 11:44:29 +0100

    Moved handling of client connection to it's own controller.

--- 07-chat/backend/src/controllers/socket_controller.ts
+++ 07-chat/backend/src/controllers/socket_controller.ts
@@ -2,6 +2,17 @@
  * Socket Controller
  */
 import Debug from 'debug'
+import { Socket } from 'socket.io'
 
 // Create a new debug instance
 const debug = Debug('chat:socket_controller')
+
+// Handle the user connecting
+export const handleConnection = (socket: Socket) => {
+	debug('🙋🏼 A user connected', socket.id)
+
+	// Handle user disconnecting
+	socket.on('disconnect', () => {
+		debug('✌🏻 A user disconnected', socket.id)
+	})
+}
